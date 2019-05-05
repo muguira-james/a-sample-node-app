cb7b57277394f5dfa1edd2d019de795f64f62729
muguira-james <muguira.james@gmail.com>
2019-05-05 14:55:38 -0400

    1st change

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+	<p>My sample node app</p>
+	<button>Click me</button>
+      </div>
+    );
+  }
+}
+
+export default App;
